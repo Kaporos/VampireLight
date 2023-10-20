56c02a39b33421ee38b1786fe3c6f4d75bbd7260
Kaporos <theo@daron.be>
2023-10-20 17:37:39 +0200

    adding start scene property to menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,18 +1,14 @@
 extends Control
 
+@export var start_scene: PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_start_pressed():
-	print("Start is pressed !")
+	get_tree().change_scene_to_packed(start_scene);
 
 
 func _on_leave_pressed():
