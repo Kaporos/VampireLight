2022dd8fbf6d52f9f0f2c8d43552c372f0cdd200
Tristan <tristan@doesnotexist.com>
2023-10-20 21:22:00 +0200

    adding moving platforms base

---
+++ components/Moving_platform.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+@export var point_list: Array[Node2D] = [];
+@export var speed: int = 300;
+@export var should_rotate : bool = false;
+@export var rotate_speed : int = 0; #rad/s
+@export var started : bool = true;
+@export var retour : bool = true;
+
+var index = 0
+var stop  = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	global_position = point_list[0].global_position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(started and (not stop)):
+		var destination = point_list[(index + 1) % point_list.size()]
+		var direction =  destination.global_position - global_position;
+		velocity = direction.normalized() * speed;
+		if(direction.length() < 5):
+			index = index + 1
+			global_position = point_list[(index) % point_list.size()].global_position;
+			if(index == point_list.size() - 1):
+				if(retour):
+					index = 0
+				else :
+					stop = true 
+		move_and_collide(velocity * delta);
+		if(should_rotate):
+			rotation = (destination.global_position.y - global_position.y)/(destination.global_position.x - global_position.x)
