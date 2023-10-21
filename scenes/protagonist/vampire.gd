0a8dcfc3e589b7bd9c0348c6676edc83930743b6
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2023-10-21 03:38:08 +0200

    Add attack animation

--- scenes/protagonist/vampire.gd
+++ scenes/protagonist/vampire.gd
@@ -14,6 +14,7 @@ var collision_offset = false
 var orientation_was_right = true
 var is_tranforming = false
 var elevate_bat = false
+var is_attacking = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -23,56 +24,71 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
+
+
 	if elevate_bat:
-		transform[2][1] -= BAT_SPEED*delta/10
+		transform[2][1] -= BAT_SPEED*delta/8
 	if is_tranforming:
 		return
 	var input_vector = get_input()
 	if is_bat:
-		move_bat()
-		animate_bat(input_vector)
+		move_bat(input_vector)
+		if not is_attacking:
+			animate_bat(input_vector)
 	else:
-		move_humanoid(delta)
-		animate_humanoid(input_vector)
+		move_humanoid(delta, input_vector)
+		if not is_attacking:
+			animate_humanoid(input_vector)
 
 	check_for_transform()
 		
-func move_bat():
+func move_bat(input_vector):
 	$HumanoidCollision.disabled = true
 	$BatCollision.disabled = false
-	var direction_horizontal = Input.get_axis("ui_left", "ui_right")
-	var direction_vertical = Input.get_axis("ui_up", "ui_down")
+	var direction_horizontal = input_vector.x
+	var direction_vertical = input_vector.y
 	velocity.x = direction_horizontal
 	velocity.y = direction_vertical
 	velocity = velocity.normalized() * BAT_SPEED
 	move_and_slide()
 
-func move_humanoid(delta):
+func move_humanoid(delta, input_vector):
 	$HumanoidCollision.disabled = false
 	$BatCollision.disabled = true
-	var input_vector = get_input()
-
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("w_pressed") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = input_vector.x
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
 func animate_bat(input_vector):
 	$HumanoidAnimation.visible = false
 	$BatAnimation.visible = true
+
+	if is_attacking:
+		if orientation_was_right:
+			$BatAnimation.play("attack_right")
+			await get_tree().create_timer(0.8).timeout
+			is_attacking = false
+			return
+		else:
+			$BatAnimation.play("attack_left")
+			await get_tree().create_timer(0.8).timeout
+			is_attacking = false
+			return
+
 	if (input_vector[0] == 0 and input_vector[1] == 0):
 		if orientation_was_right:
 			$BatAnimation.play("idle_right")
@@ -93,6 +109,21 @@ func animate_bat(input_vector):
 func animate_humanoid(input_vector):
 	$HumanoidAnimation.visible = true
 	$BatAnimation.visible = false
+
+	if Input.is_action_just_pressed("ui_accept"):
+		if orientation_was_right:
+			is_attacking = true
+			$HumanoidAnimation.play("attack_right")
+			await get_tree().create_timer(0.5).timeout
+			is_attacking = false
+			return
+		else:
+			is_attacking = true
+			$HumanoidAnimation.play("attack_left")
+			await get_tree().create_timer(0.5).timeout
+			is_attacking = false
+			return
+
 	if (input_vector[0] == 0 and input_vector[1] == 0):
 		if orientation_was_right:
 			if collision_offset:
@@ -123,9 +154,9 @@ func animate_humanoid(input_vector):
 		return
 
 func check_for_transform():
+
 	var transform_command = Input.is_action_pressed("bat_transform")
 	if transform_command:
-
 
 		if is_bat:
 			is_tranforming = true
@@ -160,4 +191,10 @@ func check_for_transform():
 			return
 
 func get_input():
-	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	return Input.get_vector("a_pressed", "d_pressed", "w_pressed", "s_pressed")
+
+func get_usefull_vector():
+	var input_vector = get_input()
+	var x_comp = input_vector[1] - input_vector[0]
+	var y_comp = input_vector[3] - input_vector[2]
+	return Vector2(x_comp, y_comp)
