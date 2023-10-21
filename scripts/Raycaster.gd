6a9bbc92ac259e06ab7c15156f25acf68d6e91ab
emec delam <kiwi.bp.surviv@gmail.com>
2023-10-22 00:28:36 +0200

    raycast update + physiclayer fix

--- scripts/Raycaster.gd
+++ scripts/Raycaster.gd
@@ -2,9 +2,9 @@ extends Node2D
 class_name ray
 
 # Called when the node enters the scene tree for the first time.
-func can_raycast(pos1, pos2) -> bool:
+func can_raycast(pos1, pos2, collision_mask) -> bool:
 	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(pos1, pos2)
+	var query = PhysicsRayQueryParameters2D.create(pos1, pos2, collision_mask)
 	var result = space_state.intersect_ray(query);
 	return !result;
 	
