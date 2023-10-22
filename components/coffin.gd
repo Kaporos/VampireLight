7c6905de095a4559e6d17da9408e4674b17bed15
Kaporos <theo@daron.be>
2023-10-22 08:44:09 +0200

    adding coffin sound on close

--- components/coffin.gd
+++ components/coffin.gd
@@ -5,7 +5,12 @@ func _on_body_entered(body:Node2D):
 	if body is Vampire:
 		body.set_physics_process(false)
 		body.visible = false
-		$AnimatedSprite2D.play_backwards("default")
-		await $AnimatedSprite2D.animation_finished
-		await get_tree().create_timer(1).timeout
-		get_tree().change_scene_to_packed(Game.next_level())
+	$AnimationPlayer.play("close")
+
+
+func coffin_anim():
+	$AnimatedSprite2D.play_backwards("default")
+
+func tp_player():
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_packed(Game.next_level())
